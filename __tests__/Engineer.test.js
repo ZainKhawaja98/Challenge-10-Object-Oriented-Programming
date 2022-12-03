9ec77963a87880c2332cf4e91fffcc197caaf102
zain khawaja <zainkhawaja98@gmail.com>
2022-12-02 19:59:45 -0500

    finished tests, lib, and src folders

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,13 +1,22 @@
-const Engineer = require("../lib/Engineer");
+const Engineer = Require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
-  //code goes here
+   //code goes here
+  const github = "github";
+  const e = new Engineer("Tim",1,"test@test.com",github);
+  expect(e.github).toBe(github);
 });
 
 test("getRole() should return \"Engineer\"", () => {
-  //code goes here
+   //code goes here
+  const testValue = "Engineer";
+  const e = new Engineer("Tim", 1, "test@test.com", "github");
+  expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get GitHub username via getGithub()", () => {
-  //code goes here
+   //code goes here
+  const testValue = "github";
+  const e = new Engineer("Tim",1,"test@test.com",testValue);
+  expect(e.getGithub()).toBe(testValue);
 });
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,15 +1,21 @@
-const Intern = require("../lib/Intern");
+const Intern = Require("../lib/Intern");
 
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const e = new Intern("Bob", 1, "test@test.com", testValue);
   expect(e.school).toBe(testValue);
 });
 
 test("getRole() should return \"Intern\"", () => {
-  //code goes here
+   //code goes here
+  const testValue = "Intern";
+  const e = new Intern("Bob", 1, "test@test.com", "school");
+  expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get school via getSchool()", () => {
-  //code goes here
+   //code goes here
+  const testValue = "school";
+  const e = new Intern("Bob", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
 });
